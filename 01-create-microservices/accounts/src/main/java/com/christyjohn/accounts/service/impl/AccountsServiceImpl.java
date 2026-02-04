1466807e8278a6e3b2af4272c7442478ccf9260a
Christy John <christyjohn.crz@gmail.com>
2026-02-04 11:56:18 +0530

    Finished Accounts application

---
+++ 01-create-microservices/accounts/src/main/java/com/christyjohn/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,69 @@
+package com.christyjohn.accounts.service.impl;
+
+import com.christyjohn.accounts.mapper.CustomerMapper;
+import com.christyjohn.accounts.constants.AccountsConstants;
+import com.christyjohn.accounts.dto.CustomerDto;
+import com.christyjohn.accounts.entity.Accounts;
+import com.christyjohn.accounts.entity.Customer;
+import com.christyjohn.accounts.exception.CustomerAlreadyExistsException;
+import com.christyjohn.accounts.repository.AccountsRepository;
+import com.christyjohn.accounts.repository.CustomerRepository;
+import com.christyjohn.accounts.service.AccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountsServiceImpl implements AccountsService  {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+    /**
+     * @param customerDto - CustomerDto Object
+     */
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                    + customerDto.getMobileNumber());
+        }
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    /**
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
+    }
+
+    @Override
+    public CustomerDto fetchAccount(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateAccount(CustomerDto customerDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAccount(String mobileNumber) {
+        return false;
+    }
+}
