ee0383212b4245529e235068124422d70266fdec
Christy John <christyjohn.crz@gmail.com>
2026-02-15 10:20:24 +0530

    Enable communication with downstream services using FeignClient

--- 05-service-registration-and-discovery/accounts/src/main/java/com/christyjohn/accounts/mapper/CustomerMapper.java
+++ 05-service-registration-and-discovery/accounts/src/main/java/com/christyjohn/accounts/mapper/CustomerMapper.java
@@ -1,5 +1,6 @@
 package com.christyjohn.accounts.mapper;
 
+import com.christyjohn.accounts.dto.CustomerDetailsDto;
 import com.christyjohn.accounts.dto.CustomerDto;
 import com.christyjohn.accounts.entity.Customer;
 
@@ -17,5 +18,12 @@ public class CustomerMapper {
         customer.setEmail(customerDto.getEmail());
         customer.setMobileNumber(customerDto.getMobileNumber());
         return customer;
+    }
+
+    public static CustomerDetailsDto mapToCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDto) {
+        customerDetailsDto.setName(customer.getName());
+        customerDetailsDto.setEmail(customer.getEmail());
+        customerDetailsDto.setMobileNumber(customer.getMobileNumber());
+        return customerDetailsDto;
     }
 }
