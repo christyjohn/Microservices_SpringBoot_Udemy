52ddc7cc61886a99c76851556e18c070aebeffa1
Christy John <christyjohn.crz@gmail.com>
2026-03-13 14:54:55 +0530

    Initialized project

---
+++ 11-event-driven-microservices-kafka/accounts/src/main/java/com/christyjohn/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,21 @@
+package com.christyjohn.accounts.mapper;
+
+import com.christyjohn.accounts.dto.AccountsDto;
+import com.christyjohn.accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+}
